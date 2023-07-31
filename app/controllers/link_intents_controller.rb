2dfca96811415dfdeee7b68ea61bed1b2d2878c1
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-07-31 08:13:21 -0400

    controladores creados

---
+++ app/controllers/link_intents_controller.rb
@@ -0,0 +1,2 @@
+class LinkIntentsController < ApplicationController
+end
---
+++ test/controllers/link_intents_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LinkIntentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
